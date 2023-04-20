9b10cbff66231e17f8bda72925b36bac784c12ab
Dennyie <rpereiracordeiro9@gmail.com>
2023-04-20 13:02:18 -0300

    Arrumando o player jump com agune

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,82 +7,46 @@ public class Move : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private GameObject player;
     [SerializeField] private Rigidbody2D rigidbody2d;
-    [SerializeField] private float maxJumpHeight;
     [SerializeField] private float initialJumpForce;
-    [SerializeField] private float jumpTime; // Tempo máximo de salto
-    private float startY;
+    [SerializeField] private float maxJumpForce;
+    private float accumulatedJumpForce;
     private float moving;
-    private bool isGrounded = false;
-    private bool canJump = true;
-    private bool hasPressedSpace = false;
-    private float jumpTimeCounter = 0f;
 
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float checkRadius;
-    [SerializeField] private LayerMask whatIsGround;
-
-    void Start()
+    private void Start()
     {
-        startY = transform.position.y;
+        accumulatedJumpForce = initialJumpForce;
     }
 
     void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        
+      /*  if (Input.GetKeyDown(KeyCode.Space))
         {
-            hasPressedSpace = true;
-            canJump = true;
-            jumpTimeCounter = jumpTime;
             rigidbody2d.AddForce(Vector2.up * initialJumpForce, ForceMode2D.Impulse);
-        }
+        } */
 
-        if (Input.GetKey(KeyCode.Space) && hasPressedSpace && canJump)
-        {
-            if (jumpTimeCounter > 0)
+        if (Input.GetKey(KeyCode.Space))
+        {         
+            if (accumulatedJumpForce < maxJumpForce)
             {
-                rigidbody2d.AddForce(Vector2.up * initialJumpForce * jumpTimeCounter, ForceMode2D.Force);
-                jumpTimeCounter -= Time.deltaTime;
-            }
-            else
-            {
-                canJump = false;
+                Debug.Log(accumulatedJumpForce);
+                rigidbody2d.AddForce(Vector2.up * accumulatedJumpForce, ForceMode2D.Impulse);
+                accumulatedJumpForce += 0.1f;
+                
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.Space) && hasPressedSpace)
+        if (Input.GetKeyUp(KeyCode.Space))
         {
-            canJump = false;
+            accumulatedJumpForce = initialJumpForce;
+            Debug.Log(accumulatedJumpForce);
         }
 
         moving = Input.GetAxisRaw("Horizontal");
 
         rigidbody2d.velocity = new Vector2(speed * moving, rigidbody2d.velocity.y);
 
-        float jumpHeight = transform.position.y - startY;
-        //Debug.Log("Jump Height: " + jumpHeight);
-
-        if (jumpHeight > startY + maxJumpHeight)
-        {
-            canJump = false;
-        }
-
-        if (isGrounded && !hasPressedSpace)
-        {
-            canJump = false;
-        }
-
-        if (!isGrounded)
-        {
-            hasPressedSpace = false;
-        }
-    }
-
-    private void OnDrawGizmosSelected() //para mostrar o tamanho do gizmos no jogo
-    {
-        if (groundCheck == null) return;
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(groundCheck.position, checkRadius);
+        
     }
 }
+
